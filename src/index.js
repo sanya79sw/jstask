ada5e9a03bac20186bcf815b5b4c1e544b4ee2b5
Alex <sanya79sw@gmail.com>
2019-07-03 00:05:18 +0300

    delete folder

--- src/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = function gradingStudents(grades) {
-    for (let i = 0; i < grades.length; i++) {
-        if (typeof grades[i] !== 'number') return new Error();
-        else if (grades[i] < 38) continue;
-        else if (grades[i] % 5 >= 3) grades[i] = grades[i] + (5 - grades[i] % 5);
-    }
-    return grades;
-}
